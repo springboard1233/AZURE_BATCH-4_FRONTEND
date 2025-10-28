052be70dda487481d4e8ac0ba53d862d00392adf
saa-nikaa <saanika604@gmail.com>
2025-10-28 23:20:33 +0530

    Removes files form main

--- src/pages/Reports.jsx
+++
@@ -1,8 +0,0 @@
-export default function Reports() {
-  return (
-    <div className="p-4 text-gray-700">
-      <h2 className="text-xl font-bold mb-2">Reports Section</h2>
-      <p>Download and view performance and forecast reports here.</p>
-    </div>
-  );
-}
