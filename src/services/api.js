723b12d315ecd6696ff3200ccc6dc93a5da446c7
Niveditha499 <nivedithadooreddy@gmail.com>
2025-10-28 17:58:52 +0530

    force delete services folder with local changes

--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-export async function uploadAzureUsage(data) {
-  // Placeholder for AzureUsage.csv upload
-  console.log("Uploading AzureUsage data:", data);
-}
-
-export async function uploadExternalFactors(data) {
-  // Placeholder for ExternalFactors.csv upload
-  console.log("Uploading ExternalFactors data:", data);
-}
